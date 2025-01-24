27fef2f2777a43e8309eb461e7c82fb4e811f62b
benchqqq <71522221+benchqqq@users.noreply.github.com>
2025-01-24 12:47:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# main.py
-print("Hello, Android!")
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Hello App"
+    page.add(ft.Text(value="Hello, Android!", size=30))
+
+ft.app(target=main)
